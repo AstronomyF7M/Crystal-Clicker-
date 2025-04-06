36adde3927be440d1adc938af327cbe9ae385f3a
AstroV3 <8769937516@student.cms.k12.nc.us>
2025-04-05 22:48:29 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,21 @@
 export default {
-    message: "Hello from config.js!"
+    message: "Hello from config.js!",
+    weather: {
+        interval: 15000, // Weather change interval (milliseconds)
+        probabilities: {
+            Clear: 0.5,
+            Rain: 0.2,
+            Snow: 0.1,
+            Storm: 0.2
+        },
+        effects: {
+            Rain: 1.5,  // 50% increase in crystal generation
+            Snow: 0.5,  // 50% decrease in crystal generation
+            Storm: 0.2, //90% decrease in crystal generation,
+            Clear: 1
+        },
+        stormDuration: 5000, // Storm duration in milliseconds
+        stormRecoveryMultiplier: 3, //Boost after storm
+        weatherControlCost: 20000 //Crystals
+    }
 };
